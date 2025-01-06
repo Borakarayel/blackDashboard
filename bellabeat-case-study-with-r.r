90d25aa4b6cc491e7c3f953ab0bd92939fa1a51d
Borakarayel <m.bora.karayel@gmail.com>
2025-01-06 11:03:50 +0100

    Kaggle Notebook | Bellabeat Case Study With R | Version 3

---
+++ bellabeat-case-study-with-r.r
@@ -0,0 +1,68 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "08bacc83",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001652,
+     "end_time": "2025-01-06T10:03:41.085839",
+     "exception": false,
+     "start_time": "2025-01-06T10:03:41.084187",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "# <span style=\"color:#FA8072\"> Bellabeat Case Study With R </span>\n",
+    "[BellaLogo](http://https://www.google.com/url?sa=i&url=https%3A%2F%2Fvicukegbu.medium.com%2Fbellabeat-case-study-912c9aa2b0fa&psig=AOvVaw2qL63Vl3MnDb5a_volC9Kk&ust=1736244158972000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwiptv3T6-CKAxWBcPEDHWc8IO0QjRx6BAgAEBk)\n",
+    "\n",
+    "## <span style=\"color:#FA8072\"> Table of Contents </span>\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [
+    {
+     "datasetId": 1041311,
+     "sourceId": 7746251,
+     "sourceType": "datasetVersion"
+    }
+   ],
+   "dockerImageVersionId": 30749,
+   "isGpuEnabled": false,
+   "isInternetEnabled": true,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.4.0"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.306068,
+   "end_time": "2025-01-06T10:03:41.209283",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2025-01-06T10:03:37.903215",
+   "version": "2.6.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
